7be0c53465e6dd56baa8870fbe11f9f7787f0894
MatthewKamal <152787257+MatthewKamal@users.noreply.github.com>
2026-03-18 20:15:36 +0200

    2nd Commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,45 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import search_icon from "../../assets/search_icon.svg";
 import bell_icon from "../../assets/bell_icon.svg";
 import profile_img from "../../assets/profile_img.png";
 import caret_icon from "../../assets/caret_icon.svg";
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navRef = useRef(null);
   const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
+    const handleScroll = () => {
       if (window.scrollY >= 80) {
         navRef.current.classList.add("nav-dark");
       } else {
         navRef.current.classList.remove("nav-dark");
       }
-    });
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
+  const handleNavClick = () => setMenuOpen(false);
 
   return (
     <div ref={navRef} className="navbar">
       <div className="navbar-left">
         <img
           src={logo}
-          alt=""
-          onClick={() => navigate("/")}
+          alt="Netflix"
+          onClick={() => {
+            navigate("/");
+            setMenuOpen(false);
+          }}
           style={{ cursor: "pointer" }}
         />
-        <ul>
+
+        <ul className="desktop-nav">
           <li>
             <NavLink to="/">Home</NavLink>
           </li>
@@ -51,17 +60,63 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
+
       <div className="navbar-right">
-        <img src={search_icon} alt="" className="icons" />
+        <img src={search_icon} alt="Search" className="icons" />
         <p>Children</p>
-        <img src={bell_icon} alt="" className="icons" />
+        <img src={bell_icon} alt="Notifications" className="icons" />
         <div className="navbar-profile">
-          <img src={profile_img} alt="" className="profile" />
+          <img src={profile_img} alt="Profile" className="profile" />
           <img src={caret_icon} alt="" />
           <div className="dropdown">
             <p>Sign Out of Netflix</p>
           </div>
         </div>
+
+        <button
+          className={`hamburger ${menuOpen ? "open" : ""}`}
+          onClick={() => setMenuOpen((prev) => !prev)}
+          aria-label="Toggle menu"
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+      </div>
+
+      <div className={`mobile-menu ${menuOpen ? "mobile-menu--open" : ""}`}>
+        <ul>
+          <li>
+            <NavLink to="/" onClick={handleNavClick}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/tvshows" onClick={handleNavClick}>
+              TV Shows
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies" onClick={handleNavClick}>
+              Movies
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/new" onClick={handleNavClick}>
+              New & Popular
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/mylist" onClick={handleNavClick}>
+              My List
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/browse" onClick={handleNavClick}>
+              Browse by Languages
+            </NavLink>
+          </li>
+        </ul>
       </div>
     </div>
   );
